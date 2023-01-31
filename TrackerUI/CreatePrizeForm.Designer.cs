280e23a2287f4b2a6cf30be09d6ac2d3abda9a8f
Domen Žukovec <zukovec22@gmail.com>
2023-01-31 15:16:57 +0100

    Dodal še dve formi za ustvarjanje nagrade in nadzorno ploščo

---
+++ TrackerUI/CreatePrizeForm.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace TrackerUI
+{
+    partial class CreatePrizeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CreatePrizeForm));
+            this.headerLabel1 = new System.Windows.Forms.Label();
+            this.PlaceNumberValueTextBox1 = new System.Windows.Forms.TextBox();
+            this.placeNumberLabel = new System.Windows.Forms.Label();
+            this.placeNameValueTextBox = new System.Windows.Forms.TextBox();
+            this.placeNameLabel = new System.Windows.Forms.Label();
+            this.priceAmountValueTextBox = new System.Windows.Forms.TextBox();
+            this.priceAmountLabel = new System.Windows.Forms.Label();
+            this.prizePercantageValueTextBox = new System.Windows.Forms.TextBox();
+            this.prizePercentageLabel = new System.Windows.Forms.Label();
+            this.orLabel = new System.Windows.Forms.Label();
+            this.createPrizeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // headerLabel1
+            // 
+            this.headerLabel1.AutoSize = true;
+            this.headerLabel1.Font = new System.Drawing.Font("Segoe UI Light", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.headerLabel1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.headerLabel1.Location = new System.Drawing.Point(90, 10);
+            this.headerLabel1.Name = "headerLabel1";
+            this.headerLabel1.Size = new System.Drawing.Size(267, 50);
+            this.headerLabel1.TabIndex = 24;
+            this.headerLabel1.Text = "Ustvari nagrado";
+            // 
+            // PlaceNumberValueTextBox1
+            // 
+            this.PlaceNumberValueTextBox1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.PlaceNumberValueTextBox1.Location = new System.Drawing.Point(240, 95);
+            this.PlaceNumberValueTextBox1.Name = "PlaceNumberValueTextBox1";
+            this.PlaceNumberValueTextBox1.Size = new System.Drawing.Size(186, 35);
+            this.PlaceNumberValueTextBox1.TabIndex = 26;
+            this.PlaceNumberValueTextBox1.TextChanged += new System.EventHandler(this.firstNameValueTextBox1_TextChanged);
+            // 
+            // placeNumberLabel
+            // 
+            this.placeNumberLabel.AutoSize = true;
+            this.placeNumberLabel.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.placeNumberLabel.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.placeNumberLabel.Location = new System.Drawing.Point(30, 92);
+            this.placeNumberLabel.Name = "placeNumberLabel";
+            this.placeNumberLabel.Size = new System.Drawing.Size(177, 37);
+            this.placeNumberLabel.TabIndex = 25;
+            this.placeNumberLabel.Text = "Mesto igralca";
+            // 
+            // placeNameValueTextBox
+            // 
+            this.placeNameValueTextBox.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.placeNameValueTextBox.Location = new System.Drawing.Point(240, 150);
+            this.placeNameValueTextBox.Name = "placeNameValueTextBox";
+            this.placeNameValueTextBox.Size = new System.Drawing.Size(186, 35);
+            this.placeNameValueTextBox.TabIndex = 28;
+            // 
+            // placeNameLabel
+            // 
+            this.placeNameLabel.AutoSize = true;
+            this.placeNameLabel.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.placeNameLabel.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.placeNameLabel.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.placeNameLabel.Location = new System.Drawing.Point(30, 148);
+            this.placeNameLabel.Name = "placeNameLabel";
+            this.placeNameLabel.Size = new System.Drawing.Size(139, 37);
+            this.placeNameLabel.TabIndex = 27;
+            this.placeNameLabel.Text = "Ime mesta";
+            // 
+            // priceAmountValueTextBox
+            // 
+            this.priceAmountValueTextBox.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.priceAmountValueTextBox.Location = new System.Drawing.Point(240, 201);
+            this.priceAmountValueTextBox.Name = "priceAmountValueTextBox";
+            this.priceAmountValueTextBox.Size = new System.Drawing.Size(186, 35);
+            this.priceAmountValueTextBox.TabIndex = 30;
+            // 
+            // priceAmountLabel
+            // 
+            this.priceAmountLabel.AutoSize = true;
+            this.priceAmountLabel.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.priceAmountLabel.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.priceAmountLabel.Location = new System.Drawing.Point(30, 199);
+            this.priceAmountLabel.Name = "priceAmountLabel";
+            this.priceAmountLabel.Size = new System.Drawing.Size(204, 37);
+            this.priceAmountLabel.TabIndex = 29;
+            this.priceAmountLabel.Text = "Znesek nagrade";
+            // 
+            // prizePercantageValueTextBox
+            // 
+            this.prizePercantageValueTextBox.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.prizePercantageValueTextBox.Location = new System.Drawing.Point(240, 316);
+            this.prizePercantageValueTextBox.Name = "prizePercantageValueTextBox";
+            this.prizePercantageValueTextBox.Size = new System.Drawing.Size(186, 35);
+            this.prizePercantageValueTextBox.TabIndex = 32;
+            // 
+            // prizePercentageLabel
+            // 
+            this.prizePercentageLabel.AutoSize = true;
+            this.prizePercentageLabel.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.prizePercentageLabel.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.prizePercentageLabel.Location = new System.Drawing.Point(30, 313);
+            this.prizePercentageLabel.Name = "prizePercentageLabel";
+            this.prizePercentageLabel.Size = new System.Drawing.Size(188, 37);
+            this.prizePercentageLabel.TabIndex = 31;
+            this.prizePercentageLabel.Text = "Delež nagrade";
+            this.prizePercentageLabel.Click += new System.EventHandler(this.prizePercentageLabel_Click);
+            // 
+            // orLabel
+            // 
+            this.orLabel.AutoSize = true;
+            this.orLabel.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.orLabel.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.orLabel.Location = new System.Drawing.Point(190, 262);
+            this.orLabel.Name = "orLabel";
+            this.orLabel.Size = new System.Drawing.Size(67, 37);
+            this.orLabel.TabIndex = 33;
+            this.orLabel.Text = "-ali-";
+            // 
+            // createPrizeButton
+            // 
+            this.createPrizeButton.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.createPrizeButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.createPrizeButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.createPrizeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.createPrizeButton.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.createPrizeButton.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.createPrizeButton.Location = new System.Drawing.Point(126, 412);
+            this.createPrizeButton.Name = "createPrizeButton";
+            this.createPrizeButton.Size = new System.Drawing.Size(194, 53);
+            this.createPrizeButton.TabIndex = 35;
+            this.createPrizeButton.Text = "Ustvari nagrado";
+            this.createPrizeButton.UseVisualStyleBackColor = true;
+            // 
+            // CreatePrizeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(462, 476);
+            this.Controls.Add(this.createPrizeButton);
+            this.Controls.Add(this.orLabel);
+            this.Controls.Add(this.prizePercantageValueTextBox);
+            this.Controls.Add(this.prizePercentageLabel);
+            this.Controls.Add(this.priceAmountValueTextBox);
+            this.Controls.Add(this.priceAmountLabel);
+            this.Controls.Add(this.placeNameValueTextBox);
+            this.Controls.Add(this.placeNameLabel);
+            this.Controls.Add(this.PlaceNumberValueTextBox1);
+            this.Controls.Add(this.placeNumberLabel);
+            this.Controls.Add(this.headerLabel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "CreatePrizeForm";
+            this.Text = "Ustvari nagrado";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label headerLabel1;
+        private TextBox PlaceNumberValueTextBox1;
+        private Label placeNumberLabel;
+        private TextBox placeNameValueTextBox;
+        private Label placeNameLabel;
+        private TextBox priceAmountValueTextBox;
+        private Label priceAmountLabel;
+        private TextBox prizePercantageValueTextBox;
+        private Label prizePercentageLabel;
+        private Label orLabel;
+        private Button createPrizeButton;
+    }
+}
---
+++ TrackerUI/TournamentDashboardForm.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace TrackerUI
+{
+    partial class TournamentDashboardForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TournamentDashboardForm));
+            this.headerLabel1 = new System.Windows.Forms.Label();
+            this.LoadExistingTournamentDropDownComboBox1 = new System.Windows.Forms.ComboBox();
+            this.loadExistingTournamentLabel = new System.Windows.Forms.Label();
+            this.loadTurnamentButton = new System.Windows.Forms.Button();
+            this.createTurnamentButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // headerLabel1
+            // 
+            this.headerLabel1.AutoSize = true;
+            this.headerLabel1.Font = new System.Drawing.Font("Segoe UI Light", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.headerLabel1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.headerLabel1.Location = new System.Drawing.Point(71, 9);
+            this.headerLabel1.Name = "headerLabel1";
+            this.headerLabel1.Size = new System.Drawing.Size(283, 50);
+            this.headerLabel1.TabIndex = 25;
+            this.headerLabel1.Text = "Nadzorna plošča";
+            this.headerLabel1.Click += new System.EventHandler(this.headerLabel1_Click);
+            // 
+            // LoadExistingTournamentDropDownComboBox1
+            // 
+            this.LoadExistingTournamentDropDownComboBox1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LoadExistingTournamentDropDownComboBox1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.LoadExistingTournamentDropDownComboBox1.FormattingEnabled = true;
+            this.LoadExistingTournamentDropDownComboBox1.Location = new System.Drawing.Point(60, 164);
+            this.LoadExistingTournamentDropDownComboBox1.Name = "LoadExistingTournamentDropDownComboBox1";
+            this.LoadExistingTournamentDropDownComboBox1.Size = new System.Drawing.Size(315, 38);
+            this.LoadExistingTournamentDropDownComboBox1.TabIndex = 27;
+            // 
+            // loadExistingTournamentLabel
+            // 
+            this.loadExistingTournamentLabel.AutoSize = true;
+            this.loadExistingTournamentLabel.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.loadExistingTournamentLabel.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.loadExistingTournamentLabel.Location = new System.Drawing.Point(140, 124);
+            this.loadExistingTournamentLabel.Name = "loadExistingTournamentLabel";
+            this.loadExistingTournamentLabel.Size = new System.Drawing.Size(153, 37);
+            this.loadExistingTournamentLabel.TabIndex = 26;
+            this.loadExistingTournamentLabel.Text = "Izberi turnir";
+            // 
+            // loadTurnamentButton
+            // 
+            this.loadTurnamentButton.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.loadTurnamentButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.loadTurnamentButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.loadTurnamentButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.loadTurnamentButton.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.loadTurnamentButton.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.loadTurnamentButton.Location = new System.Drawing.Point(130, 208);
+            this.loadTurnamentButton.Name = "loadTurnamentButton";
+            this.loadTurnamentButton.Size = new System.Drawing.Size(173, 44);
+            this.loadTurnamentButton.TabIndex = 31;
+            this.loadTurnamentButton.Text = "Naloži turnir";
+            this.loadTurnamentButton.UseVisualStyleBackColor = true;
+            // 
+            // createTurnamentButton
+            // 
+            this.createTurnamentButton.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.createTurnamentButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.createTurnamentButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.createTurnamentButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.createTurnamentButton.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.createTurnamentButton.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.createTurnamentButton.Location = new System.Drawing.Point(229, 328);
+            this.createTurnamentButton.Name = "createTurnamentButton";
+            this.createTurnamentButton.Size = new System.Drawing.Size(194, 53);
+            this.createTurnamentButton.TabIndex = 36;
+            this.createTurnamentButton.Text = "Ustvari turnir";
+            this.createTurnamentButton.UseVisualStyleBackColor = true;
+            // 
+            // TournamentDashboardForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(435, 393);
+            this.Controls.Add(this.createTurnamentButton);
+            this.Controls.Add(this.loadTurnamentButton);
+            this.Controls.Add(this.LoadExistingTournamentDropDownComboBox1);
+            this.Controls.Add(this.loadExistingTournamentLabel);
+            this.Controls.Add(this.headerLabel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "TournamentDashboardForm";
+            this.Text = "TournamentDashboardForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label headerLabel1;
+        private ComboBox LoadExistingTournamentDropDownComboBox1;
+        private Label loadExistingTournamentLabel;
+        private Button loadTurnamentButton;
+        private Button createTurnamentButton;
+    }
+}
