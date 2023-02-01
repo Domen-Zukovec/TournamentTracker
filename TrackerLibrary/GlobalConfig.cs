d987e54622aefe16f6870f6cc9bf20dcc0fe9dd7
Domen Žukovec <zukovec22@gmail.com>
2023-02-01 11:35:38 +0100

    Added GlobalConfig and IDataConnections also tryed to do a TournamentDB that didnt work out :|

---
+++ TrackerLibrary/GlobalConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public static class GlobalConfig
+    {
+        public static List<IDataConnection> Connections { get; private set; }
+        
+        public static void InicializeConnetions (bool database, bool textFiles) 
+        {
+            if (database)
+            {
+                // TODO - create SQL Connection
+            }
+            if (textFiles)
+            {
+                // TODO - Create the Text Connection
+            }
+        }
+    }
+}
