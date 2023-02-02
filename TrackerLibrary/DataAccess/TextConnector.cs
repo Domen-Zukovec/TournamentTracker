03835a8e12080033bdb4d8ea3a09e68eebfd3fcc
Domen Žukovec <zukovec22@gmail.com>
2023-02-02 15:59:34 +0100

    Started work on TextConnector :)

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -4,17 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TrackerLibrary.Models;
+using TrackerLibrary.DataAccess.TextHelpers;
 
 namespace TrackerLibrary.DataAccess
 {
     public class TextConnector : IDataConnection
     {
+        private const string PrizesFile = "PrizeModels.csv";
         // TODO - Write up the CreatePrze for text files
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
+            // Load the text file and convert the text to List<PrizeModel>
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
 
-            return model;
+            // Find the max ID
+            // Load the text file and convert the text to List<PrizeModel>
+            // Find the max ID
+            // Add the new record with the new ID (max + 1)
+            // Convert the prizes to list<string>
+            // Save the list<string> to the text file
+
         }
     }
 }
